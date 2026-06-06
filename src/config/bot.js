9345da9a9e0162586c614abe418c50ccabe08ae9
xio591 <xx4521131@gmail.com>
2026-06-06 20:17:58 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -20,14 +20,14 @@ export const botConfig = {
     // 1 = Streaming
     // 2 = Listening
     // 3 = Watching
-    // 4 = Custom
+    // 4 = Observing
     // 5 = Competing
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "Ready to help!",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 4, 
       },
     ],
   },
